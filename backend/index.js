8f398f0a265365688cbd1ec3cfc4b9ba50f609e1
Tiiso Theophilus Senosha <theotiiso@gmail.com>
2022-06-11 15:59:29 +0200

    Added functionality to convert xlsx to json

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,36 @@
 const express = require('express')
-const dotenv = require('dotenv')
-const port = process.env.PORT || 3000
-
 const app = express()
+const xlsxtojson = require("xlsx-to-json");
+const xlstojson = require("xls-to-json");
 
-app.listen(port, () => console.log(`Server started on port ${port}`))
+
+app.use(function(req, res, next) { //allow cross origin requests
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "POST, PUT, OPTIONS, DELETE, GET");
+    res.header("Access-Control-Max-Age", "3600");
+    res.header("Access-Control-Allow-Headers", "Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+    next();
+});
+
+// configuration
+app.use(express.static(__dirname + '/public'));                
+app.use('/public/uploads',express.static(__dirname + '/public/uploads'));       
+
+app.get('/', function (req, res) {
+  res.send('Hello World')
+})
+
+app.post('/api/xlstojson', function(req, res) {
+	xlsxtojson({
+		input: "./tenants.xlsx",  // input xls 
+	    output: "output.json", // output json 
+	    lowerCaseHeaders:true
+	}, function(err, result) {
+	    if(err) {
+	      res.json(err);
+	    } else {
+	      res.json(result);
+	    }
+	});
+});
+app.listen(3000)
